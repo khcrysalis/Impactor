7834b037d8e2c376601be7931c9a26d42f250189
khcrysalis <sam4r16@gmail.com>
2025-12-12 10:36:32 -0800

    feat: add sparsebox for pairing

--- crates/utils/src/options.rs
+++ crates/utils/src/options.rs
@@ -110,6 +110,7 @@ pub enum SignerApp {
     LiveContainer,
     LiveContainerAndSideStore,
     StikDebug,
+    SparseBox
 }
 
 impl SignerApp {
@@ -122,6 +123,7 @@ impl SignerApp {
             Some("com.SideStore.SideStore") => SignerApp::SideStore,
             Some("com.rileytestut.AltStore") => SignerApp::AltStore,
             Some("com.stik.js") => SignerApp::StikDebug,
+            Some("com.kdt.SparseBox") => SignerApp::SparseBox,
             _ => SignerApp::Default,
         }
     }
@@ -133,7 +135,7 @@ impl SignerApp {
     pub fn pairing_file_path(&self) -> Option<&'static str> {
         use SignerApp::*;
         match self {
-            Antrag | Feather | Protokolle | StikDebug => Some("/Documents/pairingFile.plist"),
+            Antrag | Feather | Protokolle | StikDebug | SparseBox => Some("/Documents/pairingFile.plist"),
             SideStore => Some("/Documents/ALTPairingFile.mobiledevicepairing"),
             LiveContainerAndSideStore => Some("/Documents/SideStore/Documents/ALTPairingFile.mobiledevicepairing"),
             _ => None,
