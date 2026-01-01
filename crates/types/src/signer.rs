d1206e467ea42617ca1f193eb923901d447f1cc9
se2crid <151872490+se2crid@users.noreply.github.com>
2026-01-01 17:05:51 +0100

    fix: livecontainer + sidestore

--- crates/types/src/signer.rs
+++ crates/types/src/signer.rs
@@ -34,10 +34,11 @@ impl Signer {
             return Ok(());
         }
 
-        let bundles = bundle
-            .collect_bundles_sorted()?
-            .into_iter()
+        let all_bundles = bundle.collect_bundles_sorted()?;
+        let bundles = all_bundles
+            .iter()
             .filter(|b| b.bundle_type().should_have_entitlements())
+            .cloned()
             .collect::<Vec<_>>();
 
         if let Some(new_name) = self.options.custom_name.as_ref() {
@@ -95,7 +96,11 @@ impl Signer {
                 {
                     match self.options.app {
                         SignerApp::LiveContainerAndSideStore => {
-                            if let Some(embedded_bundle) = bundles
+                            bundle.set_info_plist_key("ALTCertificateID", &**serial_number)?;
+                            fs::write(bundle.bundle_dir().join("ALTCertificate.p12"), p12_data)
+                                .await?;
+
+                            if let Some(embedded_bundle) = all_bundles
                                 .iter()
                                 .find(|b| b.bundle_dir().ends_with("SideStoreApp.framework"))
                             {
@@ -372,7 +377,7 @@ impl Signer {
         }
 
         settings.set_for_notarization(false);
-        settings.set_shallow(true);
+        settings.set_shallow(false);
 
         Ok(settings)
     }
