e37c04d92bc56a7df0e6f39902aab6c280375aef
khcrysalis <sam4r16@gmail.com>
2026-01-19 07:54:06 -0800

    fix maybe

--- apps/plumeimpactor/build.rs
+++ apps/plumeimpactor/build.rs
@@ -1,7 +1,19 @@
-fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("../../package/windows/icon.ico");
-        res.compile().unwrap();
+use std::io;
+use winres::WindowsResource;
+
+fn main() -> io::Result<()> {
+    if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows" {
+        let mut res = WindowsResource::new();
+        match std::env::var("CARGO_CFG_TARGET_ENV").unwrap().as_str() {
+            "gnu" => {
+                res.set_ar_path("x86_64-w64-mingw32-ar")
+                    .set_windres_path("x86_64-w64-mingw32-windres");
+            }
+            "msvc" => {}
+            _ => panic!("unsupported env"),
+        };
+        res.set_icon("assets/icon.ico");
+        res.compile()?;
     }
+    Ok(())
 }
